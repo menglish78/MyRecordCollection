9112ed0ec313f0f2ff9db8acf9ee8fefa1f51024
Matt English <menglish78@bellsouth.net>
2016-10-25 18:55:26 -0400

    Added Entity Framework and initial Model/Context.

---
+++ MyRecordCollection/MyRecordCollection/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyRecordCollection.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ MyRecordCollection/MyRecordCollection/Models/AlbumContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyRecordCollection.Models
+{
+    public class AlbumContext : DbContext
+    {
+        public DbSet<Album> Categories { get; set; }
+    }
+}
