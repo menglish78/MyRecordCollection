84d3d3317735c0890982d68322f97b0da919a222
Matt English <menglish78@bellsouth.net>
2016-11-07 18:38:14 -0500

    Moved AlbumController to Controller folder. Added validations to Album class.

--- MyRecordCollection/MyRecordCollection/Models/Album.cs
+++ MyRecordCollection/MyRecordCollection/Models/Album.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,12 @@ namespace MyRecordCollection.Models
     public class Album
     {
         public int AlbumID { get; set; }
+        [Required(ErrorMessage = "Please enter an album title.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter an artist.")]
         public string Artist { get; set; }
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range (1900, 3000, ErrorMessage = "Please enter a valid year.")]
         public int Year { get; set; }
         public string Label { get; set; }
         public string Format { get; set; }
